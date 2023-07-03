87cbf15991d4e3d881b2fb91a33d370bd7f9de1a
Gautam Sharma <gautam332000lavanya@gmail.com>
2023-07-03 08:06:43 +0530

    initial commit

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocialVibe
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
